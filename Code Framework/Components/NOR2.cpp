2678d91e7d29d38a263abf6d0a6c807a4b3fa142
AhmadElgohary <51035403+AhmadElgohary@users.noreply.github.com>
2019-11-28 13:52:47 +0200

    adding gates

---
+++ Code Framework/Components/NOR2.cpp
@@ -0,0 +1,44 @@
+#include "NOR2.h"
+
+NOR2::NOR2(GraphicsInfo *r_pGfxInfo, int r_FanOut):Gate(r_pGfxInfo,2, r_FanOut)
+{
+}
+
+
+void NOR2::Operate()
+{
+	//caclulate the output status as the ANDing of the two input pins
+
+	//Add you code here
+
+    // we should select here the in puts and the outputs but i think it not required to add inputs in thr GUI and get the output 
+
+}
+
+
+// Function Draw
+// Draws 2-input OR gate
+void NOR2::Draw(UI* pUI)
+{
+	//Call output class and pass gate drawing info to it.
+	pUI->DrawNOR2(*m_pGfxInfo);
+}
+
+//returns status of outputpin
+int NOR2::GetOutPinStatus()	
+{
+	return m_OutputPin.getStatus();
+}
+
+
+//returns status of Inputpin #n
+int NOR2::GetInputPinStatus(int n)	
+{
+	return m_InputPins[n-1].getStatus();	//n starts from 1 but array index starts from 0.
+}
+
+//Set status of an input pin on HIGH or LOW
+void NOR2::setInputPinStatus(int n, STATUS s)
+{
+	m_InputPins[n-1].setStatus(s);
+}
---
+++ Code Framework/Components/XNOR2.cpp
@@ -0,0 +1,44 @@
+#include "XNOR2.h"
+
+XNOR2::XNOR2(GraphicsInfo *r_pGfxInfo, int r_FanOut):Gate(r_pGfxInfo,2, r_FanOut)
+{
+}
+
+
+void XNOR2::Operate()
+{
+	//caclulate the output status as the ANDing of the two input pins
+
+	//Add you code here
+
+    // we should select here the in puts and the outputs but i think it not required to add inputs in thr GUI and get the output 
+
+}
+
+
+// Function Draw
+// Draws 2-input OR gate
+void XNOR2::Draw(UI* pUI)
+{
+	//Call output class and pass gate drawing info to it.
+	pUI->DrawXNOR2(*m_pGfxInfo);
+}
+
+//returns status of outputpin
+int XNOR2::GetOutPinStatus()	
+{
+	return m_OutputPin.getStatus();
+}
+
+
+//returns status of Inputpin #n
+int XNOR2::GetInputPinStatus(int n)	
+{
+	return m_InputPins[n-1].getStatus();	//n starts from 1 but array index starts from 0.
+}
+
+//Set status of an input pin on HIGH or LOW
+void XNOR2::setInputPinStatus(int n, STATUS s)
+{
+	m_InputPins[n-1].setStatus(s);
+}
