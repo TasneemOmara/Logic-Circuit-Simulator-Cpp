5ac168612fbaf35ffc2154eb7e68d097cbe5c40d
AhmadElgohary <51035403+AhmadElgohary@users.noreply.github.com>
2019-11-28 14:26:36 +0200

    adding led

---
+++ Code Framework/Components/Led.h
@@ -0,0 +1,19 @@
+#ifndef _GATE_H
+#define _GATE_H
+
+
+#include "InputPin.h"
+#include "OutputPin.h"
+#include "Component.h"
+
+class Led:public Component
+{
+protected:
+	InputPin m_InputputPin;	//Led has only 1 inputputPin
+public:
+	Led(GraphicsInfo* pGfxInfo);
+	
+
+};
+
+#endif
