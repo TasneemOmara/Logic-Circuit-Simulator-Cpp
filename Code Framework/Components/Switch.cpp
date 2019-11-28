60764504c3c77772f31ec5b0ff7f8e30afb752c4
TasneemOmara <tsneem.omara@gmail.com>
2019-11-28 13:59:28 +0200

    adding switch

---
+++ Code Framework/Components/Switch.cpp
@@ -0,0 +1,8 @@
+#include "Switch.h"
+
+
+//r_FanOut: Fan out of the Switch's output pin
+Switch::Switch(GraphicsInfo* pGfxInfo,int r_FanOut):Component(pGfxInfo), m_OutputPin(r_FanOut)
+{
+
+}
---
+++ Code Framework/Components/Switch.h
@@ -0,0 +1,19 @@
+#ifndef _GATE_H
+#define _GATE_H
+
+
+#include "InputPin.h"
+#include "OutputPin.h"
+#include "Component.h"
+
+class Switch:public Component
+{
+protected:
+	OutputPin m_OutputPin;	//Switch has only 1 OutputPin
+public:
+	Switch(GraphicsInfo* pGfxInfo, int r_FanOut);
+	
+
+};
+
+#endif
