dacd2d93da747a485ad8f11cb6d432cc8e2fe46f
TasneemOmara <tsneem.omara@gmail.com>
2019-12-03 20:29:21 +0200

    adding select function in select

--- Code Framework/Actions/select1.cpp
+++ Code Framework/Actions/select1.cpp
@@ -7,6 +7,20 @@ select1::select1()
 }
 
 
-select1::~select1()
-{
+bool select(ApplicationManager *App, int &index){
+    int count=0;
+    Component* CompList;
+    bool selected;
+    App->GetCompList(count, CompList);
+    for (int i = 0; i < count; i++)
+    {
+        selected = (CompList+i)->selected();
+        if (selected)
+        {
+            index = i;
+            break;
+        }        
+    }
+    return false;
+    
 }
--- Code Framework/Actions/select1.h
+++ Code Framework/Actions/select1.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Action.h"
+#include "../Components/Component.h"
 class select1: public Action
 {
 public:
 	select1();
+	bool select(ApplicationManager *App, int &index);
 	
 	~select1();
 };
