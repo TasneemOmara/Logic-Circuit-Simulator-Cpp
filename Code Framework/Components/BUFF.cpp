58dbcad705e81147883653048497268e3ec66b90
AhmadElgohary <51035403+AhmadElgohary@users.noreply.github.com>
2019-12-04 20:48:43 +0200

    adding BUFF

---
+++ Code Framework/Components/BUFF.cpp
@@ -0,0 +1,14 @@
+#include "BUFF.h"
+
+
+
+BUFF::BUFF(GraphicsInfo* pGfxInfo):Component(pGfxInfo)
+{
+	InputPin* m_InputputPin = new InputPin;
+    m_InputputPin->setComponent(this);
+}
+
+BUFF::BUFF(GraphicsInfo* pGfxInfo,int r_FanOut):Component(pGfxInfo), m_OutputPin(r_FanOut)
+{
+
+}
---
+++ Code Framework/Components/BUFF.h
@@ -0,0 +1,21 @@
+#ifndef _GATE_H
+#define _GATE_H
+
+
+#include "InputPin.h"
+#include "OutputPin.h"
+#include "Component.h"
+
+class BUFF:public Component
+{
+protected:
+	InputPin m_InputputPin;	//BUFF has only 1 inputputPin
+    OutputPin m_OutputPin;	//BUFF has only 1 OutputPin
+public:
+	BUFF(GraphicsInfo* pGfxInfo);
+    BUFF(GraphicsInfo* pGfxInfo, int r_FanOut);
+	
+
+};
+
+#endif
