511123cfaffc82cc6cbe3223b408ecc80b272428
AhmadElgohary <51035403+AhmadElgohary@users.noreply.github.com>
2019-12-05 14:46:23 +0200

    add Label

---
+++ Code Framework/Actions/ADD_Label.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "action.h"
+#include "..\Components\Connections.h"
+
+class ADD_Label :public Action
+{private: 
+	int Cx, Cy;	//Center point of the gate
+	int x1, y1, x2, y2;	//Two corners of the rectangluar area
+public:
+	ADD_Label(ApplicationManager* pApp);
+	virtual ~ADD_Label(void);
+
+	//Execute action (code depends on action type)
+	virtual void Execute();  // to get the first component and store it in  the comp1 only
+
+
+
+	virtual void Undo();
+	virtual void Redo();
+
+};
+
